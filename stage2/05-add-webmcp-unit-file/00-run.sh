43659b310c9f4c0d0006d5517a35bf494324ebda
John Boiles <johnaboiles@gmail.com>
2018-11-12 22:55:35 -0800

    Update webmcp systemd unit

--- stage2/05-add-webmcp-unit-file/00-run.sh
+++ stage2/05-add-webmcp-unit-file/00-run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
 on_chroot << EOF
-curl -fsSL https://gist.githubusercontent.com/johnboiles/da4f3fac73105c82d900e8118dae1ec4/raw/f5bf24641e7ce6c000b5d79dc0c5ed68477566f7/webmcp.service -o /etc/systemd/system/webmcp.service
+curl -fsSL https://gist.githubusercontent.com/johnboiles/da4f3fac73105c82d900e8118dae1ec4/raw -o /etc/systemd/system/webmcp.service
 systemctl enable webmcp
 EOF
